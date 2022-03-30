bf3ab18b399e9cda0264f562e91ae68b1343b742
DanielD26 <103547416@student.swin.edu.au>
2022-03-30 15:01:31 +1100

    angular pages done

--- Frontend/nba_app/src/app/app.module.ts
+++ Frontend/nba_app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTeamsComponent } from './components/create-teams/create-teams.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTeamsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/nba_app/src/app/models/Main.ts
@@ -0,0 +1,9 @@
+import { Team } from "./Team"
+
+export class Main {
+    Team: Team[] = []
+
+    constructor() {
+        this.Team = []
+    }
+}
