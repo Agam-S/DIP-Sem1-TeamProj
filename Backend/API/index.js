cc8ed9a857688ce09091af6d095229328ac11361
Agam Singh <agamsingh@Agams-MacBook-Pro.local>
2022-05-09 21:11:17 +1000

    algorithm works endpoint

--- Backend/API/index.js
+++ Backend/API/index.js
@@ -14,20 +14,6 @@ const player = require("./routes/player");
 // Importing team model
 const TeamRouter = require("./models/team");
 
-// let runPy = new Promise(function (success, nosuccess) {
-//   const { spawn } = require("child_process");
-//   let player_names;
-//   const pyprog = spawn("python", ["./ml.py", player_names]);
-
-//   pyprog.stdout.on("data", function (data) {
-//     success(data);
-//   });
-
-//   pyprog.stderr.on("data", (data) => {
-//     nosuccess(data);
-//   });
-// });
-
 // MongoDB Connection
 mongoose.connect(
   "mongodb+srv://admin:admin1234@prac.r7c5f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -54,9 +40,9 @@ const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// app.get("/", (req, res) => {
-//   res.send({ message: "Welcome to the NBA API" });
-// });
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to the NBA API" });
+});
 
 app.get("/:_id", async (req, res) => {
   const foundTeam = await TeamRouter.findById(req.params._id);
