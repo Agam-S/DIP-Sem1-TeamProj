a18ffad26ff2308bc87dea1fa2930a7721a3be36
Xander-Rouse <103590876@student.swin.edu.au>
2022-06-06 11:11:29 +1000

    refactor decoded token

---
+++ Backend/API/routes/decodeToken.js
@@ -0,0 +1,13 @@
+const jwt_decode = require("jwt-decode")
+
+function decodeToken(authToken) {
+
+    const token = authToken;
+
+    const token1 = token.substring(7, token.length);
+  
+    const sub = jwt_decode(token1).sub;
+
+    return sub;
+}
+module.exports = decodeToken;
