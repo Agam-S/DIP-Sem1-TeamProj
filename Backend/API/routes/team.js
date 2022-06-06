171e5b24e7aebf6503501af1caf0c73602b835c8
Xander-Rouse <103590876@student.swin.edu.au>
2022-06-06 10:35:41 +1000

    Added user to Get Put and Post

--- Backend/API/routes/team.js
+++ Backend/API/routes/team.js
@@ -3,11 +3,15 @@ const router = require("express").Router();
 // Importing team model
 const team = require("../models/team");
 const jwtCheck = require("./verifyToken");
+const jwt_decode = require("jwt-decode")
 
 // Routes
 router.get("/all", async (req, res) => {
+  const token = req.headers.authorization;
+  const token1 = token.replace("Bearer ", "");
+  const sub = jwt_decode(token1).sub;
   try {
-    const foundTeam = await team.find({});
+    const foundTeam = await team.find({user: sub});
     res.json(foundTeam);
   } catch (err) {
     res.json({ message: err });
@@ -15,11 +19,15 @@ router.get("/all", async (req, res) => {
 });
 
 router.post("/create", async (req, res) => {
+
+  const token = req.headers.authorization;
+  const token1 = token.replace("Bearer ", "");
+  const sub = jwt_decode(token1).sub;
   try {
     const newTeam = new team({
       teamName: req.body.teamName,
       players: req.body.players,
-      user: req.headers["id_token"]
+      user: sub
     });
     const savedTeam = await newTeam.save();
     res.json(savedTeam);
@@ -38,10 +46,14 @@ router.get("/view/:_id", async (req, res) => {
 });
 
 router.put("/edit/:_id", async (req, res) => {
+  const token = req.headers.authorization;
+  const token1 = token.replace("Bearer ", "");
+  const sub = jwt_decode(token1).sub;
   try {
     const putTeam = await team.findByIdAndUpdate(req.params._id, {
       teamName: req.body.teamName,
       players: req.body.players,
+      user: sub
     });
     res.json(putTeam);
   } catch (err) {
